4ea74dc5937181e0e40b241bfe64efbab1c0cdc1
Michel Allan <michel_allan20xx@hotmail.com>
2014-01-29 18:09:28 -0200

    header e inicio da segunda sessao feitos

---
+++ js/contagem_clientes.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  contagem_iniciada_clientes = false;
+  function iniciar_contagem_clientes() {
+    if (!contagem_iniciada_clientes) {
+      for (var i = 0; i <= 25; i++) {
+        (function(i) {
+          setTimeout(function(){
+            $(".contagem-clientes").html(i);
+            $(".clientes").append('<img src="img/cliente.png" class="boneco-clientes">');
+          },i*100)
+        })(i);
+      };
+    };
+    contagem_iniciada_clientes = true;
+  }
+
+  $(window).on("scroll", function() {
+    var body_height = $("body").height();
+    var scroll = $(document).scrollTop();
+    var scroll_responsive = (100*scroll/body_height)
+    if (scroll_responsive >= 4.5 && scroll_responsive <= 6.5) {
+      iniciar_contagem_clientes();
+    };
+  });
+})
---
+++ js/contagem_horas_programacao.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  contagem_iniciada_horas = false;
+  function iniciar_contagem_horas() {
+    if (!contagem_iniciada_horas) {
+      for (var i = 0; i <= 16640; i++) {
+        (function(i) {
+          setTimeout(function(){
+            $(".contagem-horas").html(i);
+          },i*50)
+        })(i);
+      };
+    };
+    contagem_iniciada_horas = true;
+  }
+
+  $(window).on("scroll", function() {
+    var body_height = $("body").height();
+    var scroll = $(document).scrollTop();
+    var scroll_responsive = (100*scroll/body_height)
+    if (scroll_responsive >= 9 && scroll_responsive <= 10) {
+      iniciar_contagem_horas();
+    };
+  });
+})
